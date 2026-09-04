8df3269c5491a10da2c10d1204bb8a6c767c7224
heisenstack <dinaniamine@gmail.com>
2026-09-04 12:32:00 +0000

    Project init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Captcha } from './pages/captcha/captcha';
+import { Result } from './pages/result/result';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'captcha', component: Captcha },
+  { path: 'result', component: Result },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/pages/result/result.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-result',
+  styleUrl: './result.scss',
+  templateUrl: './result.html',
+})
+export class Result {}
