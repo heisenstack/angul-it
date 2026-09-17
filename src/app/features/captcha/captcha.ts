569e254858322f11c699adfdb92196e44429fe95
heisenstack <dinaniamine@gmail.com>
2026-09-17 18:51:15 +0100

    Adding captcha component

--- src/app/features/captcha/captcha.ts
+++ src/app/features/captcha/captcha.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProgressService } from '../../core/services/progress';
+import {FormsModule} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   selector: 'app-captcha',
   styleUrl: './captcha.scss',
   templateUrl: './captcha.html',
 })
-export class Captcha {}
+export class Captcha {
+  router = inject(Router);
+  progress = inject(ProgressService);
+  answer = '';
+  result : 'correct' | 'incorrect' | null = null;
+
+  submit(): void{
+    const isRight = this.progress.isCorrect(this.answer);
+    this.result = isRight ? 'correct' : 'incorrect';
+    if (isRight) {
+      this.progress.advance();
+      this.answer = '';
+      this.result = null;
+      if (this.progress.isFinished) {
+        this.router.navigate(['/result']);
+      }
+    }
+  }
+}
