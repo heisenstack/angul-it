00c9236dd751854150c406fa1314f84d873208c2
heisenstack <dinaniamine@gmail.com>
2026-09-17 18:50:08 +0100

    Adding progress services for tracking the progress where we are standing

---
+++ src/app/core/services/progress.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {Challenge} from '../models/challenge.model';
+
+const CHALENGE: Challenge[] = [
+   { id: 'math-1',prompt: '7+5',answer: '12',},
+   { id: 'math-2',prompt: '9-3',answer: '6',},
+   { id: 'text-1', prompt: 'Type the word "cat" backwards', answer: 'tac' },
+]
+
+@Injectable({providedIn: 'root'})
+export class ProgressService{
+    private challenges = CHALENGE;
+    currentIndex = 0;
+
+    get currentChallenge(): Challenge{
+        return this.challenges[this.currentIndex]?? null;
+    }
+
+    get isFinished(): boolean{
+        return this.currentIndex >= this.challenges.length;
+    }
+    // currentChallenge: Challenge = CHALENGE;
+
+    isCorrect(submitted: string): boolean {
+        const challenge= this.currentChallenge;
+        if (!challenge) return false;
+        return submitted.trim() === challenge.answer;
+    }
+    advance(): void{
+        this.currentIndex++;
+    }
+}
